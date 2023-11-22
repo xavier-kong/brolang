0fdeb9c3586573009dc4ca4af76d1f457f0c98e8
xavier-kong <kongxianchengxavier@gmail.com>
2023-11-22 21:39:06 +0800

    box?

--- src/traverse.rs
+++ src/traverse.rs
@@ -1,5 +1,20 @@
 use crate::Node;
 
 pub fn traverse(node: Node) {
-    println!("{:?}", node);
+    println!("{:?}", node.data.slice);
+
+    let nodes: Vec<Node> = vec![node];
+
+    while nodes.len() != 0 {
+        let new_nodes: Vec<Node> = vec![];
+
+        for (i, n) in nodes.iter().enumerate() {
+            println!("{:?}", n);
+            if n.left.is_some() {
+                new_nodes.push(n.left);
+            }
+            new_nodes.push(n.left);
+            new_nodes.push(n.right);
+        }
+    }
 }
