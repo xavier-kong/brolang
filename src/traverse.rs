fbee2cfd7e3684dbb7984ab60bf3b3a0c4330e20
xavier-kong <kongxianchengxavier@gmail.com>
2023-11-24 20:24:12 +0800

    compiler is your friend

--- src/traverse.rs
+++ src/traverse.rs
@@ -3,25 +3,27 @@ use crate::Node;
 pub fn traverse(node: Node) {
     println!("{:?}", node.data.slice);
 
-    let nodes: Vec<Node> = vec![node];
+    let mut nodes: Vec<&Node> = vec![&node];
 
     while nodes.len() != 0 {
-        let new_nodes: Vec<Node> = vec![];
+        let mut new_nodes: Vec<&Node> = vec![];
 
-        for (i, n) in nodes.iter().enumerate() {
+        for (_i, n) in nodes.iter().enumerate() {
             println!("{:?}", n);
-            if n.left.is_some() {
-                match n.left {
-                    Some(val) => new_nodes.push(*val),
-                    None => ()
-                }
+
+            match &n.left {
+                Some(val) => new_nodes.push(val),
+                None => ()
             }
-            if n.right.is_some()  {
-                match n.right {
-                    Some(val) => new_nodes.push(*val),
-                    None => ()
-                }
+            match &n.right {
+                Some(val) => new_nodes.push(val),
+                None => ()
             }
         }
+
+        nodes = new_nodes;
+        println!("{:?}", nodes);
     }
+
+    println!("done");
 }
