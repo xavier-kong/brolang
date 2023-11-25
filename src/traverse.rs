0332df14224e40654571c242eafb2684585789c8
xavier-kong <kongxianchengxavier@gmail.com>
2023-11-25 16:42:03 +0800

    confused

--- src/traverse.rs
+++ src/traverse.rs
@@ -3,22 +3,24 @@ use crate::Node;
 pub fn traverse(node: Node) {
     println!("{:?}", node.data.slice);
 
-    let mut nodes: Vec<&Node> = vec![&node];
+    let mut nodes: Vec<Option<Box<Node>>> = vec![Some(Box::new(node))];
 
     while nodes.len() != 0 {
-        let mut new_nodes: Vec<&Node> = vec![];
+        let mut new_nodes: Vec<Option<Box<Node>>> = vec![];
 
         for (_i, n) in nodes.iter().enumerate() {
             println!("{:?}", n);
 
-            match &n.left {
-                Some(val) => new_nodes.push(val),
-                None => ()
-            }
-            match &n.right {
-                Some(val) => new_nodes.push(val),
-                None => ()
-            }
+            let curr = n;
+
+            let val = match curr {
+                Some(val) => {
+                    return val;
+                    new_nodes.push(val.left);
+                    new_nodes.push(val.right);
+                },
+                None => new_nodes.push(None)
+            };
         }
 
         nodes = new_nodes;
